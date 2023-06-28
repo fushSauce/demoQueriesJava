09a736d2eaf71ffd75e4c06dc22b8c0335ea7072
Micky Snadden <snaddemick@ecs.vuw.ac.nz>
2023-06-28 16:10:54 +1200

    small changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,6 @@ public class Main {
     public static Connection connection = null;
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:./sample.db");
             ResultSet resultSet = getJobs("Plumbing", 90, "Wellington", 1, 5);
@@ -28,16 +27,14 @@ public class Main {
                     String columnValue = resultSet.getString(i);
                     System.out.print(columnValue + " " + rsmd.getColumnName(i));
                 }
-                System.out.println("");
+                System.out.println();
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
-    // num stars
-    // num years
 
-    // two main queries - job search from tradep perspective, tradesp search from client perspective
+    // two main queries - job search from tradesp perspective, tradesp search from client perspective
     public static ResultSet getJobs(String specialty,double maxCost,String location, int requiredRating,int yearsExpReq)
         throws SQLException {
         Statement statement = connection.createStatement();
@@ -52,42 +49,6 @@ public class Main {
         return statement.executeQuery(query);
     }
 
-    public static String getJobStr(ResultSet rs) throws SQLException {
-        StringBuilder sb = new StringBuilder();
-        int jId = rs.getInt("JId");
-        int sId = rs.getInt("SId");
-        int cId = rs.getInt("CId");
-        int maxCost = rs.getInt("MaxCost");
-        int yearsExpRequired = rs.getInt("yearsExpRequired");
-        String location = rs.getString("Location");
-        String title = rs.getString("Title");
-        String jobInfo = rs.getString("JobInfo");
-        String invoice = rs.getString("Invoice");
-        sb.append(jId)
-            .append(",")
-            .append(sId)
-            .append(",")
-            .append(cId)
-            .append(",")
-            .append(maxCost)
-            .append(",")
-            .append(yearsExpRequired)
-            .append(",")
-            .append(location)
-            .append(",")
-            .append(title)
-            .append(",")
-            .append(jobInfo)
-            .append(",")
-            .append(invoice);
-        return sb.toString();
-    }
-//    yearsExpRequired INTEGER CHECK (yearsExpRequired > 0),
-//    ratingRequired INTEGER,
-//    Location VARCHAR(255),
-//    Title    VARCHAR(255),
-//    JobInfo  VARCHAR(255),
-//    Invoice  VARCHAR(255),
     public static void getTradesPeople(String specialty,int ratingAtOrAbove,String location) {
     }
 
